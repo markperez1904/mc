c83d954a9dc9e820134cd61e875cf2a05584404b
Mark Perez <34041737+markperez1904@users.noreply.github.com>
2019-04-13 20:59:22 -0700

    Add files via upload

--- mc.sh
+++ mc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /opt/minecraft && java -Xms1G -Xmx1G -jar /opt/minecraft/forge-1.12.2-14.23.5.2768-installer.jar
+cd /opt/minecraft && java -Xms1G -Xmx1G -jar /opt/minecraft/forge-1.12.2-14.23.5.2768-universal.jar
